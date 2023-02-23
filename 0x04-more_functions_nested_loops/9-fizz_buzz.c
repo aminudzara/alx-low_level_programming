129cf808aded89b6a50f0be268ca83d3792d6f31
Aminu Dzara <aminudzara@gmail.com>
2023-02-23 14:21:54 -0800

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n != 100)
+		{
+			if (n % 3 == 0)
+				printf("Fizz ");
+			else if (n % 5 == 0)
+				printf("Buzz ");
+			else if (n % 3 == 0 && n % 5 == 0)
+				printf("FizzBuzz ");
+			else
+				printf("%d ", n);
+		}
+			else
+		{
+			printf("Buzz\n");
+		}
+	}
+	return (0);
+}
