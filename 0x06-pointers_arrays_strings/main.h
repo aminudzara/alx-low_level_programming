1a51eaab63f73677db4ea0b7cfe1218af923483a
Aminu Dzara <aminudzara@gmail.com>
2023-03-02 21:46:29 +0000

    Function that copies a string

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+char *_strncpy(char *dest, char *src, int n);
 int _putchar(char);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
