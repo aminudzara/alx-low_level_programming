134739ff8d576f016192b424fde207427f7b024a
Aminu Dzara <aminudzara@gmail.com>
2023-03-16 08:40:05 +0000

    Add function that frees a previously allocated grid

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,5 +8,6 @@ int _strlen(char *s);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void print_grid(int **grid, int width, int height);
+void free_grid(int **grid, int height);
 
 #endif
