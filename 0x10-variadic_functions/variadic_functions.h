7b241aa58497cf4eed3368c77f38468e4c948875
Aminu Dzara <aminudzara@gmail.com>
2023-03-26 08:11:33 +0000

    Add function that print anything

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,21 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * struct format - struct format
+ *
+ */
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list pa);
+} print_t;
 
 #endif /* #define VARIADIC_FUNCTIONS_H */
