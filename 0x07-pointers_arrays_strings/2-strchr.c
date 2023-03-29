fb83954f70b516435fd9aff0c1dc6dcfc17b9d81
Aminu Dzara <aminudzara@gmail.com>
2023-03-29 09:43:05 +0000

    Update

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -15,12 +15,10 @@ char *_strchr(char *s, char c)
 	if (s == NULL)
 		return (NULL);
 
-	i = 0;
-	while (*(s + i) != '\0')
+	for (i = 0; *(s + i) >= '\0'; i++)
 	{
 		if (*(s + i) == c)
 			return (s + i);
-		i++;
 	}
-	return ('\0');
+	return (NULL);
 }
