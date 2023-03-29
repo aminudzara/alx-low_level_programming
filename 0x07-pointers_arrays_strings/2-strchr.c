c92111b40e8f7e412b14e12c5a80a0ced46b8c30
Aminu Dzara <aminudzara@gmail.com>
2023-03-29 09:35:06 +0000

    Add function that returns the first occurrence of a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: the string to search
+ * @c: the character to look for
+ * Return: On success - the first occurrence of @c in @s
+ * On failure - NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	if (s == NULL)
+		return (NULL);
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+			return (s + i);
+		i++;
+	}
+	return (NULL);
+}
