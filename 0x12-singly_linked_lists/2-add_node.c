c69954de590bed6e2d61d21cf01fd9a8969af4f2
Aminu Dzara <aminudzara@gmail.com>
2023-03-30 12:03:06 +0000

    Fix error

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -19,7 +19,14 @@ list_t *add_node(list_t **head, const char *str)
 	if (new == NULL)
 		return (NULL);
 
-	new->str = strdup(str);
+	dup_str = strdup(str);
+	if (dup_str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->str = dup_str;
 	new->len = _strlen(str);
 	new->next = *head;
 	*head = new;
