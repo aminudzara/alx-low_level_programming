6bc17b2760281b0bf237039afa5f250e0aca92bf
Aminu Dzara <aminudzara@gmail.com>
2023-04-03 16:32:47 +0000

    Fix memory leak

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,13 +9,14 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *tmp;
+	int n;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return (0);
 
 	tmp = *head;
-
-	*head = (*head)->next;
-
-	return (tmp->n);
+	*head = tmp->next;
+	n = tmp->n;
+	free(tmp);
+	return (n);
 }
