a4bc80b4394170a5e970ab6b89704c94e3e53f36
Aminu Dzara <aminudzara@gmail.com>
2023-04-04 19:47:47 +0000

    Fix code style

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		listint_t *current = *head;
 
-		for (i = 0; i < idx -1 && current != NULL; i++)
+		for (i = 0; i < idx - 1 && current != NULL; i++)
 			current = current->next;
 		if (current == NULL)
 			return (NULL);
